3bd2fc3b8697dcded55411f9b0454df3a863d61a
Alexander <hash_nam@mail.ru>
2023-12-28 16:11:28 +0500

    egorka-mahorka

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,37 +1,56 @@
 import React from "react";
+import { data, data2, data3 } from "./response";
 
-const test = []
-
-const Table = ({data}) => {
-    return (
-        <table >
-            <thead>
-                <tr className="heading">
-                    <th className="table-name">Название</th>
-                    <th className="table-date">Дата публикации</th>
-                    <th className="table-url">Ссылка</th>
-                    <th className="table-price">Цена</th>
-                    <th className="table-deviation">Отклонение цены от среднего %</th>
-                    <th className="table-city">Город</th>
-                    <th className="table-region">Регион</th>
-                </tr>
-            </thead>
-            <tbody>
-                {/* {data.map((item) => {
-                    return item;
-                })} */}
-                    <tr>
-                        <td className='table-name'>Название.</td>
-                        <td className="table-date">Дата публикации</td>
-                        <td className="table-url">Ссылка</td>
-                        <td className="table-price">Цена</td>
-                        <td className="table-deviation">Отклонение</td>
-                        <td className="table-city">Город</td>
-                        <td className="table-region">Регион</td>
-                    </tr>
-            </tbody>
-        </table>
-    )
-}
+const Table = ({ conter }) => {
+  return (
+    <table>
+      <thead>
+        <tr className="heading">
+          <th className="table-name">Название</th>
+          <th className="table-date">Дата публикации</th>
+          <th className="table-url">Ссылка</th>
+          <th className="table-price">Цена</th>
+          <th className="table-deviation">Отклонение цены от среднего %</th>
+          <th className="table-city">Город</th>
+        </tr>
+      </thead>
+      <tbody>
+        {conter === 1 &&
+          data.advertisements.map((t) => (
+            <tr>
+              <td className="table-name">{t.title}</td>
+              <td className="table-date">Дата публикации</td>
+              <td className="table-url">{t.url}</td>
+              <td className="table-price">{t.price}</td>
+              <td className="table-deviation">{t.priceDeviation}</td>
+              <td className="table-city">{t.city}</td>
+            </tr>
+          ))}
+        {conter === 2 &&
+          data2.advertisements.map((t) => (
+            <tr>
+              <td className="table-name">{t.title}</td>
+              <td className="table-date">Дата публикации</td>
+              <td className="table-url">{t.url}</td>
+              <td className="table-price">{t.price}</td>
+              <td className="table-deviation">{t.priceDeviation}</td>
+              <td className="table-city">{t.city}</td>
+            </tr>
+          ))}
+        {conter === 3 &&
+          data3.advertisements.map((t) => (
+            <tr>
+              <td className="table-name">{t.title}</td>
+              <td className="table-date">Дата публикации</td>
+              <td className="table-url">{t.url}</td>
+              <td className="table-price">{t.price}</td>
+              <td className="table-deviation">{t.priceDeviation}</td>
+              <td className="table-city">{t.city}</td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Table;
