b5ec494e8346489e94c584309c09b7198447c812
Rudovsky Egor <rudovskiy45@mail.ru>
2023-12-29 22:23:31 +0500

    3кт

--- src/response.js
+++ src/response.js
@@ -1,87 +1,407 @@
 export const data = {
-  averagePrice: 6723.4,
-  advertisements: [
+  "medianPrice": 2794.5,
+  "advertisements": [
     {
-      title: "Tissot T101.417.33.031.00, золотой,серебряный.(109 в Кургане",
-      url: "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/tissot_t101.417.33.031.00_zolotoyserebryanyy.109_3478081853",
-      city: "Курган",
-      price: 24999,
-      priceDeviation: 272,
-      publicationDate: "2023-12-22T21:55:46.725541+05:00",
+      "title": "Детские смарт-часы Geozon в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/detskie_smart-chasy_geozon_3630570454",
+      "city": "Курган",
+      "price": 3990,
+      "priceDeviation": 43,
+      "publicationDate": "2023-12-29T12:17:04.6225153+05:00"
     },
     {
-      title: "Смарт часы в Кургане",
-      url: "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/smart_chasy_3584055849?slocation=635730",
-      city: "Курган",
-      price: 1900,
-      priceDeviation: -72,
-      publicationDate: "2023-12-22T21:55:46.727518+05:00",
+      "title": "Смарт часы DT no 1 8 ultra 49мм в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/smart_chasy_dt_no_1_8_ultra_49mm_3762687464",
+      "city": "Курган",
+      "price": 1800,
+      "priceDeviation": -36,
+      "publicationDate": "2023-12-24T22:17:04.6244972+05:00"
     },
     {
-      title: "Apple watch se 2022 44mm в Шадринске",
-      url: "https://www.avito.ru/shadrinsk/chasy_i_ukrasheniya/apple_watch_se_2022_44mm_3617510556?slocation=635730",
-      city: "Шадринск",
-      price: 18000,
-      priceDeviation: 168,
-      publicationDate: "2023-12-23T13:55:46.729441+05:00",
+      "title": "Tissot T101.417.33.031.00, золотой,серебряный.(109 в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/tissot_t101.417.33.031.00_zolotoyserebryanyy.109_3478081853",
+      "city": "Курган",
+      "price": 24999,
+      "priceDeviation": 795,
+      "publicationDate": "2023-12-29T12:17:04.6263399+05:00"
     },
     {
-      title: "Apple Watch Ultra в Кургане",
-      url: "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/apple_watch_ultra_3786450813?slocation=635730",
-      city: "Курган",
-      price: 2390,
-      priceDeviation: -64,
-      publicationDate: "2023-12-17T21:55:46.7311127+05:00",
+      "title": "Часы Cuervo y Sorbinos Prominente 01012 в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_cuervo_y_sorbinos_prominente_01012_3613292932",
+      "city": "Курган",
+      "price": 399000,
+      "priceDeviation": 14178,
+      "publicationDate": "2023-12-29T16:17:04.6283723+05:00"
     },
     {
-      title: "Смарт часы в Шумихе",
-      url: "https://www.avito.ru/shumiha/chasy_i_ukrasheniya/smart_chasy_3587136061?slocation=635730",
-      city: "Шумиха",
-      price: 1000,
-      priceDeviation: -85,
-      publicationDate: "2023-12-23T16:55:46.7326389+05:00",
+      "title": "Apple watch 9 ultra 2 в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/apple_watch_9_ultra_2_3603453600",
+      "city": "Курган",
+      "price": 3490,
+      "priceDeviation": 25,
+      "publicationDate": "2023-12-24T22:17:04.6302378+05:00"
     },
     {
-      title: "Умные часы Samsung Galaxy Watch4 Classic 46 мм к2 в Кургане",
-      url: "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/umnye_chasy_samsung_galaxy_watch4_classic_46_mm_k2_3510229789",
-      city: "Курган",
-      price: 6495,
-      priceDeviation: -3,
-      publicationDate: "2023-12-16T21:55:46.7342185+05:00",
+      "title": "Apple watch 7 в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/apple_watch_7_2533237417",
+      "city": "Курган",
+      "price": 1800,
+      "priceDeviation": -36,
+      "publicationDate": "2023-12-24T22:17:04.6320794+05:00"
     },
     {
-      title: "Часы apple watch 3 42mm в Кургане",
-      url: "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_apple_watch_3_42mm_3542399759?slocation=635730",
-      city: "Курган",
-      price: 10000,
-      priceDeviation: 49,
-      publicationDate: "2023-12-23T15:55:46.7356758+05:00",
+      "title": "Apple watch 7 в оригинальной упаковке в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/apple_watch_7_v_originalnoy_upakovke_2675629677",
+      "city": "Курган",
+      "price": 2499,
+      "priceDeviation": -11,
+      "publicationDate": "2023-12-24T22:17:04.6339468+05:00"
     },
     {
-      title: "Часы mi band 5 в Кургане",
-      url: "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_mi_band_5_3787733386",
-      city: "Курган",
-      price: 1000,
-      priceDeviation: -85,
-      publicationDate: "2023-12-23T16:55:46.7371962+05:00",
+      "title": "Apple watch 7 в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/apple_watch_7_2579781177",
+      "city": "Курган",
+      "price": 1999,
+      "priceDeviation": -28,
+      "publicationDate": "2023-12-24T22:17:04.6358014+05:00"
     },
     {
-      title: "Часы мужские. Браслеты. Набор в Кургане",
-      url: "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_muzhskie._braslety._nabor_3661655679?slocation=635730",
-      city: "Курган",
-      price: 350,
-      priceDeviation: -95,
-      publicationDate: "2023-12-23T20:55:46.7386656+05:00",
+      "title": "Frederique Constant Runabout FC-303RV6B6 в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/frederique_constant_runabout_fc-303rv6b6_3581646668",
+      "city": "Курган",
+      "price": 89000,
+      "priceDeviation": 3085,
+      "publicationDate": "2023-12-29T16:17:04.6377648+05:00"
     },
     {
-      title: "Xiami Mi Band 6 в Кургане",
-      url: "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/xiami_mi_band_6_3921851112?slocation=635730",
-      city: "Курган",
-      price: 1100,
-      priceDeviation: -84,
-      publicationDate: "2023-12-23T15:55:46.7402058+05:00",
+      "title": "Apple watch series 9 45mm в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/apple_watch_series_9_45mm_3602837110",
+      "city": "Курган",
+      "price": 3490,
+      "priceDeviation": 25,
+      "publicationDate": "2023-12-24T22:17:04.6396079+05:00"
     },
-  ],
+    {
+      "title": "Часы Longines L 5.650.3 в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_longines_l_5.650.3_3452988543",
+      "city": "Курган",
+      "price": 20900,
+      "priceDeviation": 648,
+      "publicationDate": "2023-12-29T16:17:04.6417211+05:00"
+    },
+    {
+      "title": "Apple Watch Ultra в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/apple_watch_ultra_3786450813",
+      "city": "Курган",
+      "price": 2790,
+      "priceDeviation": 0,
+      "publicationDate": "2023-12-27T22:17:04.6437351+05:00"
+    },
+    {
+      "title": "Apple Watch X8 Pro в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/apple_watch_x8_pro_3496224691",
+      "city": "Курган",
+      "price": 2799,
+      "priceDeviation": 0,
+      "publicationDate": "2023-12-28T22:17:04.645766+05:00"
+    },
+    {
+      "title": "Подарочный набор Apple Х8 6в1 в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/podarochnyy_nabor_apple_h8_6v1_2661116965",
+      "city": "Курган",
+      "price": 3900,
+      "priceDeviation": 40,
+      "publicationDate": "2023-12-24T22:17:04.6476516+05:00"
+    },
+    {
+      "title": "Часы apple watch 8 41 mm в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_apple_watch_8_41_mm_2995482428",
+      "city": "Курган",
+      "price": 2200,
+      "priceDeviation": -21,
+      "publicationDate": "2023-12-24T22:17:04.649586+05:00"
+    },
+    {
+      "title": "Часы L Duchen D172.41.31 в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_l_duchen_d172.41.31_3612758627",
+      "city": "Курган",
+      "price": 7490,
+      "priceDeviation": 168,
+      "publicationDate": "2023-12-29T16:17:04.651701+05:00"
+    },
+    {
+      "title": "Часы СССР в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_sssr_2751090018",
+      "city": "Курган",
+      "price": 500,
+      "priceDeviation": -82,
+      "publicationDate": "2023-12-29T13:17:04.6535688+05:00"
+    },
+    {
+      "title": "Детские часы с gps в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/detskie_chasy_s_gps_3565347347",
+      "city": "Курган",
+      "price": 3000,
+      "priceDeviation": 7,
+      "publicationDate": "2023-12-29T19:17:04.6554067+05:00"
+    },
+    {
+      "title": "Фитнес браслет xiaomi mi band 6 в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/fitnes_braslet_xiaomi_mi_band_6_3531252257",
+      "city": "Курган",
+      "price": 2000,
+      "priceDeviation": -28,
+      "publicationDate": "2023-12-29T20:17:04.6573604+05:00"
+    },
+    {
+      "title": "Amazfit bip 3 в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/amazfit_bip_3_3736407998",
+      "city": "Курган",
+      "price": 2500,
+      "priceDeviation": -11,
+      "publicationDate": "2023-12-29T12:17:04.6592837+05:00"
+    },
+    {
+      "title": "Часы apple watch 3 38 mm в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_apple_watch_3_38_mm_3696199589",
+      "city": "Курган",
+      "price": 8000,
+      "priceDeviation": 186,
+      "publicationDate": "2023-12-24T22:17:04.6611101+05:00"
+    },
+    {
+      "title": "Часы Samsung Galaxy watch 42 mm розовое золото в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_samsung_galaxy_watch_42_mm_rozovoe_zoloto_2341862879",
+      "city": "Курган",
+      "price": 9000,
+      "priceDeviation": 222,
+      "publicationDate": "2023-12-28T22:17:04.6629694+05:00"
+    },
+    {
+      "title": "Смарт часы honor magic watch 2 в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/smart_chasy_honor_magic_watch_2_3631206545",
+      "city": "Курган",
+      "price": 11000,
+      "priceDeviation": 294,
+      "publicationDate": "2023-12-29T21:22:04.6648675+05:00"
+    },
+    {
+      "title": "Smart Watch x8 pro в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/smart_watch_x8_pro_3609488829",
+      "city": "Курган",
+      "price": 2899,
+      "priceDeviation": 4,
+      "publicationDate": "2023-12-28T22:17:04.6668076+05:00"
+    },
+    {
+      "title": "Смарт часы/ Х8 pro в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/smart_chasy_h8_pro_3632229790",
+      "city": "Курган",
+      "price": 1499,
+      "priceDeviation": -46,
+      "publicationDate": "2023-12-27T22:17:04.6686412+05:00"
+    },
+    {
+      "title": "Часы наручные в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_naruchnye_3651999717",
+      "city": "Курган",
+      "price": 120,
+      "priceDeviation": -96,
+      "publicationDate": "2023-12-29T11:17:04.6704802+05:00"
+    },
+    {
+      "title": "Часы apple watch 8 41 mm новые в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_apple_watch_8_41_mm_novye_3586739039",
+      "city": "Курган",
+      "price": 2490,
+      "priceDeviation": -11,
+      "publicationDate": "2023-12-28T22:17:04.672484+05:00"
+    },
+    {
+      "title": "Часы в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_3321841118",
+      "city": "Курган",
+      "price": 0,
+      "priceDeviation": -100,
+      "publicationDate": "2023-12-29T13:17:04.6742826+05:00"
+    },
+    {
+      "title": "Часы casio в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_casio_3581994675",
+      "city": "Курган",
+      "price": 349,
+      "priceDeviation": -88,
+      "publicationDate": "2023-12-26T22:17:04.6761833+05:00"
+    },
+    {
+      "title": "Умные часы Samsung Watch4 Classic 46 мм black к2 в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/umnye_chasy_samsung_watch4_classic_46_mm_black_k2_3510079193",
+      "city": "Курган",
+      "price": 7995,
+      "priceDeviation": 186,
+      "publicationDate": "2023-12-22T22:17:04.6782091+05:00"
+    },
+    {
+      "title": "Часы apple watch 5 40 mm в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_apple_watch_5_40_mm_3412382429",
+      "city": "Курган",
+      "price": 14000,
+      "priceDeviation": 401,
+      "publicationDate": "2023-12-29T13:17:04.6801532+05:00"
+    },
+    {
+      "title": "Часы настольные СССР на батарейках в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_nastolnye_sssr_na_batareykah_2199764786",
+      "city": "Курган",
+      "price": 600,
+      "priceDeviation": -79,
+      "publicationDate": "2023-12-29T21:17:04.6820163+05:00"
+    },
+    {
+      "title": "Часы в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_3954009773",
+      "city": "Курган",
+      "price": 4200,
+      "priceDeviation": 50,
+      "publicationDate": "2023-12-28T22:17:04.6839661+05:00"
+    },
+    {
+      "title": "Мужские наручные часы Dalas новые в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/muzhskie_naruchnye_chasy_dalas_novye_3607167289",
+      "city": "Курган",
+      "price": 350,
+      "priceDeviation": -87,
+      "publicationDate": "2023-12-27T22:17:04.6857881+05:00"
+    },
+    {
+      "title": "Мужские наручные часы электронные в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/muzhskie_naruchnye_chasy_elektronnye_3564812619",
+      "city": "Курган",
+      "price": 7000,
+      "priceDeviation": 150,
+      "publicationDate": "2023-12-28T22:17:04.6876702+05:00"
+    },
+    {
+      "title": "Часы с эмблемой \"Курганмашзавод\" в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_s_emblemoy_kurganmashzavod_2082634531",
+      "city": "Курган",
+      "price": 3000,
+      "priceDeviation": 7,
+      "publicationDate": "2023-12-28T22:17:04.6899078+05:00"
+    },
+    {
+      "title": "Часы Xiaomi watch s1 active в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_xiaomi_watch_s1_active_3632993950",
+      "city": "Курган",
+      "price": 7500,
+      "priceDeviation": 168,
+      "publicationDate": "2023-12-29T15:17:04.6918841+05:00"
+    },
+    {
+      "title": "Смарт часы honor к03 в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/smart_chasy_honor_k03_3819713082",
+      "city": "Курган",
+      "price": 3999,
+      "priceDeviation": 43,
+      "publicationDate": "2023-12-29T19:17:04.6939902+05:00"
+    },
+    {
+      "title": "Часы наручные в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_naruchnye_2601700506",
+      "city": "Курган",
+      "price": 999,
+      "priceDeviation": -64,
+      "publicationDate": "2023-12-22T22:17:04.695984+05:00"
+    },
+    {
+      "title": "Смарт часы в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/smart_chasy_3575409209",
+      "city": "Курган",
+      "price": 500,
+      "priceDeviation": -82,
+      "publicationDate": "2023-12-15T22:17:04.697816+05:00"
+    },
+    {
+      "title": "Часы на газоразрядных индикаторах ин-12 в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_na_gazorazryadnyh_indikatorah_in-12_3661722806",
+      "city": "Курган",
+      "price": 3000,
+      "priceDeviation": 7,
+      "publicationDate": "2023-12-28T22:17:04.6997035+05:00"
+    },
+    {
+      "title": "Часы мужские okami в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_muzhskie_okami_3706740037",
+      "city": "Курган",
+      "price": 7500,
+      "priceDeviation": 168,
+      "publicationDate": "2023-12-28T22:17:04.7015235+05:00"
+    },
+    {
+      "title": "Смарт часы x9 pro(30) в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/smart_chasy_x9_pro30_3585157683",
+      "city": "Курган",
+      "price": 1699,
+      "priceDeviation": -39,
+      "publicationDate": "2023-12-25T22:17:04.7034705+05:00"
+    },
+    {
+      "title": "Smart watch в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/smart_watch_3295341394",
+      "city": "Курган",
+      "price": 1500,
+      "priceDeviation": -46,
+      "publicationDate": "2023-12-28T22:17:04.7053469+05:00"
+    },
+    {
+      "title": "Apple watch 9 в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/apple_watch_9_3644799933",
+      "city": "Курган",
+      "price": 6000,
+      "priceDeviation": 115,
+      "publicationDate": "2023-12-24T22:17:04.7073226+05:00"
+    },
+    {
+      "title": "Смарт часы apple watch 7 закос в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/smart_chasy_apple_watch_7_zakos_3117529826",
+      "city": "Курган",
+      "price": 1500,
+      "priceDeviation": -46,
+      "publicationDate": "2023-12-29T14:17:04.7092071+05:00"
+    },
+    {
+      "title": "Часы СССР в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_sssr_2954325099",
+      "city": "Курган",
+      "price": 500,
+      "priceDeviation": -82,
+      "publicationDate": "2023-12-27T22:17:04.7110556+05:00"
+    },
+    {
+      "title": "Часы женские. Бижутерия. Набор в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_zhenskie._bizhuteriya._nabor_3542342634",
+      "city": "Курган",
+      "price": 350,
+      "priceDeviation": -87,
+      "publicationDate": "2023-12-28T22:17:04.7129507+05:00"
+    },
+    {
+      "title": "Ремешки Apple Watch силиконовые и миланская сетка в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/remeshki_apple_watch_silikonovye_i_milanskaya_setka_3554168617",
+      "city": "Курган",
+      "price": 200,
+      "priceDeviation": -93,
+      "publicationDate": "2023-12-28T22:17:04.7149687+05:00"
+    },
+    {
+      "title": "Часы в Кургане",
+      "url": "https://www.avito.ru/kurgan/chasy_i_ukrasheniya/chasy_2669745809",
+      "city": "Курган",
+      "price": 300,
+      "priceDeviation": -89,
+      "publicationDate": "2023-12-29T20:17:04.7168389+05:00"
+    }
+  ]
 };
 
 export const data2 = {
